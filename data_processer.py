271a63e8c406ff0c0ef7c8253e611737aca9d8bd
Zackary <zspink@nea.k12.ar.us>
2022-01-21 15:21:36 -0600

    sd

--- data_processer.py
+++ data_processer.py
@@ -5,12 +5,28 @@ def main():
     file_content = get_data_csv("weather_lr.csv")
 
     db = database()
+    db.create_tables(file_content[0])
 
 
 class database:
     def __init__(self):
-        self.connection = sqlite3.connect("workshops.db")
+        self.connection = sqlite3.connect("database.db")
         self.c = self.connection.cursor()
+
+    def create_tables(self, headers):
+        self.c.execute(
+            f"""CREATE TABLE IF NOT EXISTS data (
+                id INTEGER PRIMARY KEY,
+                {headers[0]} TEXT NOT NULL,
+                {headers[1]} TEXT NOT NULL,
+                {headers[2]} TEXT NOT NULL,
+                {headers[3]} TEXT NOT NULL,
+                {headers[4]} TEXT NOT NULL,
+                {headers[5]} TEXT NOT NULL,
+                {headers[6]} TEXT NOT NULL,
+                {headers[7]} TEXT NOT NULL
+            );"""
+        )
 
 
 def get_data_csv(file):
